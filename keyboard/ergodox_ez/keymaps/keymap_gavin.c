ac546cf7b7b7d15aa682110f202998ead5057cfd
Gavin Wahl <gwahl@fusionbox.com>
2017-05-15 11:17:04 -0600

    add lock screen key

--- keyboard/ergodox_ez/keymaps/keymap_gavin.c
+++ keyboard/ergodox_ez/keymaps/keymap_gavin.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| Hyper|           | Alt  |------+------+------+------+------+--------|
  * | LShift |   X  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |Alt   | Alt  | Alt  |                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |Grv/L1|  '"  |Alt   | Alt  | Alt  |                                       |  L1  | Alt  |   [  |   ]  | LOCK |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Ins  |  Del |
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(1),
         M(1),          KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,   MO(SYMB),
-        LT(SYMB,KC_GRV),M(1),      KC_RALT,  KC_RALT,KC_RALT,
+        LT(SYMB,KC_GRV),M(1),      KC_LALT,  KC_LALT,KC_LALT,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(2),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
              MO(2),    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,   KC_RSFT,
-                                  MO(SYMB),  KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
+                                  MO(SYMB),  KC_RALT,KC_LBRC,KC_RBRC,          ACTION_MODS_KEY(MOD_LGUI, KC_L),
              KC_INS,        KC_DELETE,
              KC_PGUP,
              KC_PGDN, KC_DELETE, KC_ENT
